b70211f978fdcec96fc20befc46c19a7b8e9c8ad
Erica Jiang <115647009+yuchenie@users.noreply.github.com>
2025-09-07 10:04:50 -0400

    incorporate adc throttle reading

--- cope.c
+++ cope.c
@@ -9,30 +9,35 @@
 #include "hardware/adc.h"
 #include "hardware/uart.h"
 #include "pico/time.h"
-
-#define UART_ID uart0
-#define BAUD_RATE 115200
 #define LED 25
 
+// UART to Pi. Handles sending sensor data.
+#define UART_ID uart0
+#define BAUD_RATE 115200
+
+// Hall sensor inputs.
 #define NUM_INPUTS 3
 const uint input_pins[NUM_INPUTS] = {13, 14, 15};
-const uint period_us = 50;
 
+// V/Hz linear mapping values.
 const float vphz = 0.016;
-const float max_throttle = 0;
+const float b = 4;
 
-struct repeating_timer timer;
-
+// Velocity calculation parameters.
 volatile uint64_t prev_us = 0;
 volatile uint64_t current_us = 0;
 volatile float velocity_rpm = 0;
 volatile uint pulses = 0;
-volatile float throttle = 0.5;
 
+// Throttle input.
+volatile float throttle = 0.0;
+#define THROTTLE_SENSOR 28
+
+// PWM GPIO.
 #define WRAPVAL 5000
-// #define CLKDIV 25.0f
 // #define CLKDIV 41.67f // 3000 RPM
 #define CLKDIV 83.33f // 1500 RPM
+
 #define PWM_TEST 16
 uint slice_test;
 
@@ -56,18 +61,8 @@ void on_pwm_wrap() {
     pwm_clear_irq(pwm_gpio_to_slice_num(PWM_OUT));
 }
 
-bool timer_callback(__unused struct repeating_timer *t) {
-    // 4 electrical cycles per revolution, resolution = 6 pulses per electrical rev, so 1 pulse is 1/24 rpm
-    velocity_rpm = (pulses/24) / (period_us * 1e-6);
-    // pulses = 0;
-    return true;
-}
-
-void irq_handler(uint gpio, uint32_t events) {
-    // prev_us = current_us;
-    // current_us = time_us_64();
-    
-    // debounce not necessary because only 1 bit changes at a time
+void irq_handler(uint gpio, uint32_t events) {    
+    // debounce not necessary because only 1 bit changes at a time.
     pulses++;
 }
 
@@ -111,8 +106,10 @@ void initialize() {
     gpio_set_irq_enabled(input_pins[1], GPIO_IRQ_EDGE_RISE | GPIO_IRQ_EDGE_FALL, true);
     gpio_set_irq_enabled(input_pins[2], GPIO_IRQ_EDGE_RISE | GPIO_IRQ_EDGE_FALL, true);
 
-    // update velocity every 50 microseconds. negative indicates that it's 50us between starting.
-    // add_repeating_timer_us(-50, timer_callback, NULL, &timer);
+    adc_init();
+    adc_gpio_init(THROTTLE_SENSOR);
+    adc_select_input(2);
+    throttle = adc_read() / 4095;
 }
 
 uint8_t read() {
@@ -126,22 +123,39 @@ uint8_t read() {
     return value;
 }
 
+float read_throttle() {
+    adc_select_input(2);
+    throttle = adc_read() / 4095;
+}
+
+float constrain(float value, float min, float max) {
+    if (value > max) { 
+        return max; 
+    } else if (value < min) {
+        return min;
+    } else {
+        return value;
+    }
+}
+
 int main() {
     stdio_init_all();
     initialize();
 
     while (true) {
         uint8_t input_value = read();
-
         // print_uint8(pulses);
 
+        read_throttle();
         current_us = time_us_64();
         velocity_rpm = ((float)pulses/24) / ((float) (current_us - prev_us) / 60 * 1e-6);
-        float max_voltage = velocity_rpm * vphz + max_throttle; // add constrain to 48
-        float duty_cycle = (throttle * max_voltage) / 48.0;
+        float max_voltage = velocity_rpm * vphz + b;
+        float raw_duty_cycle = (throttle * max_voltage) / 48.0;
+        float duty_cycle = constrain(raw_duty_cycle, 0.0, 1.0);
         pwm_set_chan_level(slice_out, PWM_CHAN_A, (int)(duty_cycle*WRAPVAL));
 
-        print_float(duty_cycle);
+        print_float(velocity_rpm);
+        // print_float(throttle);
 
         prev_us = current_us;
         pulses = 0;
